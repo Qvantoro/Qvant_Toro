c115b91f0d372d5a1cea0ac25b1ea1a211066e68
Qvantoro <xwanku@gmail.com>
2023-12-08 09:37:40 +0700

    Commit 1

---
+++ form/aksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$age = $_POST["age"];
+
+echo "<h2> Form data yang diambil : </h2>";
+echo "<p> $name </p>";
+echo "<p> $email </p>";
+echo "<p> $age </p>";
+
+?>
